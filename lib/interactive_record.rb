90438c7026cec42b4ef1f1fd81a06930d6aa3bb2
Matt Farmer <mrfarmer777@gmail.com>
2018-06-12 04:29:18 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -67,7 +67,7 @@ class InteractiveRecord
   end
 
   def self.find_by_name(name)
-    sql="SELECT * from #{self.table_name} WHERE name = '#{name}''" #Gotta wrap these interpolations in single quotes!!
+    sql="SELECT * from #{self.table_name} WHERE name = '#{name}'" #Gotta wrap these interpolations in single quotes!!
     DB[:conn].execute(sql)
   end
 
