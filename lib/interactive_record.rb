8dee75e7c984fda035e07445d4dacfe0708f6044
Matt Farmer <mrfarmer777@gmail.com>
2018-06-12 03:31:50 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -20,9 +20,17 @@ class InteractiveRecord
     table_info.each do |row|
       column_names<<row["name"]  #thanks to returning hashes, we can just ask for the column name for each column (stored here as a row)
     end
-    column_names.compact #adding in compact, just incase there are nils or unnamed columns.
+    column_names.compact #adding in compact, just incase there are nils or unnamed columns.      
   end
 
+  #Dynamic initialize method takes in a hash as options for initialization
+  def initialize(options={})
+    options.each do |property,value|    #for each key/value pair in the options hash passed in
+      self.send("#{property}=",value)   #call self.PROPERTY=value to create 'setters' for each option (getters or attr_accessors assigned in the child class)
+    end
+  end
 
-
+  
+  
+  
 end
