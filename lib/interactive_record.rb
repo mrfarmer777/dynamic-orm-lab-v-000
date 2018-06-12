a0ea8b280c8507d384d542fdec4fb1166d58affe
Matt Farmer <mrfarmer777@gmail.com>
2018-06-12 04:01:07 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -62,7 +62,7 @@ class InteractiveRecord
   def save
     sql="INSERT INTO #{table_name_for_insert}(#{col_names_for_insert}) VALUES (#{values_for_insert})"
     DB[:conn].execute(sql)
-    @id=DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")
+    @id=DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}").flatten  #!forgot to flatten!!
   end
 
 
